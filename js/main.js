69900128dcacd5dd265a3ed3d559334c846fd267
szzzzzzz <szzz8246@gmail.com>
2015-04-04 00:48:58 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-function sub(th) {
-localStorage.li=document.getElementById("li").value;
-console.log(localStorage.li);
-}
+angular.module('app', []).controller('con', function($scope) {
+        $scope.li=localStorage.li || "";
+        $scope.lh=localStorage.li
+        $scope.ss=localStorage.ss || 0;
+        $scope.txt=""
+        $scope.sub=function() {
+            localStorage.li=$scope.li
+            $scope.lh=localStorage.li
+        }
+        $scope.s=function(x) {
+    localStorage.ss=Number($scope.ss+x)
+    $scope.ss=Number(localStorage.ss)
+    $scope.txt="增加 "+x+"元\n"+$scope.txt
+        }
+    $scope.d=function() {
+        localStorage.clear();
+    }
+    })
