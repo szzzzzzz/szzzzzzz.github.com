e28c7c72eddfd761ac663d79c9ade6f706962333
szzzzzzz <szzz8246@gmail.com>
2015-07-03 18:53:56 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,9 +10,14 @@ angular.module('app', []).controller('con', function($scope) {
         $scope.s=function(x) {
     localStorage.ss=Number($scope.ss+x)
     $scope.ss=Number(localStorage.ss)
-    $scope.txt="增加 "+x+"元\n"+$scope.txt
+    $scope.txt="增加 "+x+"武器文明度\n"+$scope.txt
         }
     $scope.d=function() {
         localStorage.clear();
     }
+    $scope.second=function() {
+            $scope.s(1);
+　 setTimeout("$scope.second( )", 1000);
+}
+$scope.second
     })
